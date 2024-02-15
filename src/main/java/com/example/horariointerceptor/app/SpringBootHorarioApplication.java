f7412f902f81365e57800694289ebf21639c839e
psychovelcro <juanreyfotos@gmail.com>
2024-02-15 13:35:26 +0100

    Changes

--- src/main/java/com/example/horariointerceptor/app/SpringBootHorarioApplication.java
+++ src/main/java/com/example/horariointerceptor/app/SpringBootHorarioApplication.java
@@ -3,6 +3,9 @@ package com.example.horariointerceptor.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+Comment
+*/
 @SpringBootApplication
 public class SpringBootHorarioApplication {
 
