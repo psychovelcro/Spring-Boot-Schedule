2c771871b4e4694303b8c87e2f645327bda8b4a7
psychovelcro <juanreyfotos@gmail.com>
2024-02-16 09:18:06 +0100

    Javadoc

--- src/main/java/com/example/horariointerceptor/app/controllers/appController.java
+++ src/main/java/com/example/horariointerceptor/app/controllers/appController.java
@@ -2,6 +2,10 @@ package com.example.horariointerceptor.app.controllers;
 
 import java.util.Calendar;
 
+/**
+ * Este es una prueba de comment para probar git
+ */
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
